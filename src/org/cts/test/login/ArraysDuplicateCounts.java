d777bc77de30d2aef512f61b33094c117880bb11
Gwthmchlm <gowthamkalyan17@gmail.com>
2019-12-15 13:51:25 +0530

    Successfully imported ArrayPrograms

---
+++ src/org/cts/test/login/ArraysDuplicateCounts.java
@@ -0,0 +1,28 @@
+package org.cts.test.login;
+
+import java.util.Scanner;
+
+public class ArraysDuplicateCounts {
+	public static void main(String[] args)
+	{
+	int n, count=0;
+	Scanner s = new Scanner(System.in);
+	System.out.print("Enter no. of elements you want in array: ");
+	n = s.nextInt();
+	int a[] = new int[n];
+	System.out.println("Enter all the numbers: ");
+	for (int i = 0; i < n; i++)
+	{
+	a[i] = s.nextInt();
+	}
+	for (int i = 0; i < n; i++)
+	{
+	for (int j = i + 1; j < n; j++)
+	{
+	if(a[i]==a[j]) {
+	count++;
+	}
+	}}
+	System.out.println(count);
+	}
+}
---
+++ src/org/cts/test/login/LengthOfArray.java
@@ -0,0 +1,21 @@
+package org.cts.test.login;
+
+public class LengthOfArray {
+
+	// TO FIND THE LENGTH OF THE STRING
+	public static void main(String[] args) {
+		int a[]=new int[8];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=90;
+		a[4]=10;
+		a[5]=10;
+		a[6]=40;
+		a[7]=50;
+		int ar = a.length;
+		System.out.println(ar);
+		
+	}
+	
+}
